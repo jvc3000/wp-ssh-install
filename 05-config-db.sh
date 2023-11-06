06a582f8fff43111981bf38b5f111c951987dd46
vcampbell3 <vince.campbell@gmail.com>
2023-11-06 15:49:02 +0000

    moved files around

--- 05-config-db.sh
+++ 05-config-db.sh
@@ -4,16 +4,27 @@ echo "============================================"
 echo "Configure database"
 echo "============================================"
 
+PW="My9d0gly!"
+
 # Database variables
-user="wp_user"
-pass="wordpress123513"
-dbname="wp_db"
+user="wp_user01"
+pass="vxe8MXN-yvh6vet.qvk"
+dbname="wp_db01"
+
+RESULT_VARIABLE="$(mysql --defaults-extra-file=config.cnf -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$user')")"
+
+if [ "$RESULT_VARIABLE" = 1 ]; then
+echo "WARNING: The DB user name $user already exist in the DB"
+else
+  echo "The DB user name $user does not exist in the DB"
+fi
+
 echo "Creating database..."
-mysql -e "CREATE DATABASE $dbname;"
+mysql --defaults-extra-file=config.cnf -e "CREATE DATABASE $dbname;"
 echo "Creating new user..."
-mysql -e "CREATE USER '$user'@'%' IDENTIFIED BY '$pass';"
-echo "User successfully created!"
+mysql --defaults-extra-file=config.cnf -e "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';"
+# echo "User successfully created!"
 echo "Setting user privileges..."
-mysql -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$user'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql --defaults-extra-file=config.cnf -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$user'@'localhost';"
+mysql --defaults-extra-file=config.cnf -e "FLUSH PRIVILEGES;"
 echo "Success :)"
---
+++ 10-wordpress-install-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "============================================"
+echo "Install Dependencies"
+echo "============================================"
+
+# Install Apache2 web server
+echo "Install Apache2 web server"
+apt install apache2 \
+            ghostscript \
+            libapache2-mod-php
+
+# Install MySQL database
+echo "Install MySQL database"
+apt install mysql-server -y
+
+# Install php & modules
+echo "Install php & modules"
+apt install php \
+            php-bcmath \
+            php-curl \
+            php-imagick \
+            php-intl \
+            php-json \
+            php-mbstring \
+            php-mysql \
+            php-xml \
+            php-zip
+
+echo -e "\nSuccess?"

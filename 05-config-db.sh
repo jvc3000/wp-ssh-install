5906847f006197c70ee5cb2f66935a988809c553
vcampbell3 <vince.campbell@gmail.com>
2023-11-06 21:53:59 +0000

    added color to echo out

--- 05-config-db.sh
+++ 05-config-db.sh
@@ -29,11 +29,22 @@ mysql --defaults-extra-file=config.cnf -e "FLUSH PRIVILEGES;"
 echo "Success :)"
 
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-echo -e "${RED}###############################################"
-echo -e "Configuration Information"
-echo -e "Database name: $DB_NAME"
-echo -e "Database user name: $DB_USER"
-echo -e "Database user passowrd: $DB_PASS"
-echo -e "###############################################${NC}"
+# You can use these ANSI escape codes:
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+echo -e "${RED}###############################################${NC}"
+echo -e "         Configuration Information"
+echo -e "   Database name: ${BLUE}$DB_NAME${NC}"
+echo -e "     DB username: ${BLUE}$DB_USER${NC}"
+echo -e "DB user passowrd: ${BLUE}$DB_PASS${NC}"
+echo -e "${RED}###############################################${NC}"

71bab68feac1a527365aa739d5a5554359b91ad8
vcampbell3 <vince.campbell@gmail.com>
2023-11-06 20:50:18 +0000

    cleanup

--- 05-config-db.sh
+++ 05-config-db.sh
@@ -5,37 +5,35 @@ echo "Configure database"
 echo "============================================"
 
 # Database variables
-user="wp_user01"
-pass="vxe8MXN-yvh6vet.qvk"
-dbname="wp_db01"
+DB_USER="wp_user01"
+DB_PASS="vxe8MXN-yvh6vet.qvk"
+DB_NAME="wp_db01"
 
 # Error checking that can be used later
-RESULT_VARIABLE="$(mysql --defaults-extra-file=config.cnf -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$user')")"
+RESULT_VARIABLE="$(mysql --defaults-extra-file=config.cnf -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$DB_USER')")"
 
 if [ "$RESULT_VARIABLE" = 1 ]; then
-echo "WARNING: The DB user name $user already exist in the DB"
+echo "WARNING: The DB user name $DB_USER already exist in the DB"
 else
-  echo "The DB user name $user does not exist in the DB"
+  echo "The DB user name $DB_USER does not exist in the DB"
 fi
 
 echo "Creating database..."
-mysql --defaults-extra-file=config.cnf -e "CREATE DATABASE $dbname;"
+mysql --defaults-extra-file=config.cnf -e "CREATE DATABASE $DB_NAME;"
 echo "Creating new user..."
-mysql --defaults-extra-file=config.cnf -e "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';"
+mysql --defaults-extra-file=config.cnf -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 # echo "User successfully created!"
 echo "Setting user privileges..."
-mysql --defaults-extra-file=config.cnf -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$user'@'localhost';"
+mysql --defaults-extra-file=config.cnf -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
 mysql --defaults-extra-file=config.cnf -e "FLUSH PRIVILEGES;"
 echo "Success :)"
 
-bold=$(tput bold)
-normal=$(tput sgr0)
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo -e "${RED}###############################################${NC}"
-echo -e "${bold}Configuration Info${normal}"
-echo -e "Database name: $dbname"
-echo -e "Database user name: $user"
-echo -e "Database user passowrd: $pass"
-echo -e "${RED}###############################################${NC}"
+echo -e "${RED}###############################################"
+echo -e "Configuration Information"
+echo -e "Database name: $DB_NAME"
+echo -e "Database user name: $DB_USER"
+echo -e "Database user passowrd: $DB_PASS"
+echo -e "###############################################${NC}"

ea84c2f1476a6077f73df3193aa906e2d49b0907
vcampbell3 <vince.campbell@gmail.com>
2023-11-06 20:31:32 +0000

    working db and user config setup

--- 05-config-db.sh
+++ 05-config-db.sh
@@ -4,13 +4,12 @@ echo "============================================"
 echo "Configure database"
 echo "============================================"
 
-PW="My9d0gly!"
-
 # Database variables
 user="wp_user01"
 pass="vxe8MXN-yvh6vet.qvk"
 dbname="wp_db01"
 
+# Error checking that can be used later
 RESULT_VARIABLE="$(mysql --defaults-extra-file=config.cnf -sse "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$user')")"
 
 if [ "$RESULT_VARIABLE" = 1 ]; then
@@ -28,3 +27,15 @@ echo "Setting user privileges..."
 mysql --defaults-extra-file=config.cnf -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$user'@'localhost';"
 mysql --defaults-extra-file=config.cnf -e "FLUSH PRIVILEGES;"
 echo "Success :)"
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${RED}###############################################${NC}"
+echo -e "${bold}Configuration Info${normal}"
+echo -e "Database name: $dbname"
+echo -e "Database user name: $user"
+echo -e "Database user passowrd: $pass"
+echo -e "${RED}###############################################${NC}"

bc4a9b57a33c08b03d2d604c737fe2ae08b4cd7f
vcampbell3 <vince.campbell@gmail.com>
2023-11-07 06:57:00 +0000

    merge wp config

--- 00-wordpress-install.sh
+++ 00-wordpress-install.sh
@@ -4,9 +4,9 @@
 WEBSITE_DOMAIN="carolinatech.org"
 
 # Database variables
+DB_NAME="wp_db01"
 DB_USER="wp_user01"
 DB_PASS="vxe8MXN-yvh6vet.qvk"
-DB_NAME="wp_db01"
 
 echo "============================================"
 echo "Install Dependencies"
@@ -155,3 +155,23 @@ echo -e "${RED}################################################${NC}"
 # *************************************************************
 #                     END NEW TEST BLOCK
 # *************************************************************
+
+#create wp config
+cp /srv/www/carolinatech.org/wp-config-sample.php /srv/www/carolinatech.org/wp-config.php
+chown -R www-data:www-data /srv/www/carolinatech.org/wp-config.php
+#set database details with perl find and replace
+perl -pi -e "s/database_name_here/$DB_NAME/g" /srv/www/carolinatech.org/wp-config.php
+perl -pi -e "s/username_here/$DB_USER/g" /srv/www/carolinatech.org/wp-config.php
+perl -pi -e "s/password_here/$DB_PASS/g" /srv/www/carolinatech.org/wp-config.php
+#set WP salts
+perl -i -pe'
+  BEGIN {
+    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+  }
+  s/put your unique phrase here/salt()/ge
+' /srv/www/carolinatech.org/wp-config.php
+#create uploads folder and set permissions
+mkdir /srv/www/carolinatech.org/wp-content/uploads
+chmod 775 /srv/www/carolinatech.org/wp-content/uploads

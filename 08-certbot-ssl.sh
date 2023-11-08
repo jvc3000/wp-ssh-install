88df6dc84e73fabd6dd64c88a609f5e0f28572c1
“Vince <vince.campbell@gmail.com>
2023-11-08 03:21:16 +0000

    updates files

--- 08-certbot-ssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "SSL generate with certbot"
-apt install certbot python3-certbot-apache -y
-certbot run -n --apache --agree-tos -d carolinatech.org,www.carolinatech.org -m admin@carolinatech.org  --redirect
-echo "========================="
-echo "Installation is complete."
-echo "=========================" 
---
+++ s04-certbot-ssl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "SSL generate with certbot"
+apt install certbot python3-certbot-apache -y
+certbot run -n --apache --agree-tos -d carolinatech.org,www.carolinatech.org -m admin@carolinatech.org  --redirect
